c18ec35947362bb9a6078b508c1a924256eb43b1
Peter <schmidt.multimedia@gmail.com>
2021-06-07 12:18:11 +0200

    update ++

---
+++ Drone.java
@@ -0,0 +1,5 @@
+public class Drone extends HoneyBee{
+    
+
+    
+}
---
+++ HoneyBee.java
@@ -0,0 +1,12 @@
+public class HoneyBee {
+
+    public String fly() {
+        return "I believe, I can fly!";
+    }
+
+    public String doYourJob() {
+        return "I'm a Honeybee!";
+    }
+
+
+}
